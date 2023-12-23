4532b6bb3fbfd48392a91e47aca98ee4d0dc4551
hawan105 <hawn105@ajou.ac.kr>
2023-12-24 01:15:32 +0900

    Plus story, plus mainmenu sound, change background music ..etc

--- MENU/MainMenu.gd
+++ MENU/MainMenu.gd
@@ -8,9 +8,14 @@ func _on_options_pressed():
 	get_tree().change_scene_to_file("res://MENU/Options.tscn")
 
 
+
 func _on_exit_pressed():
 	get_tree().quit()
 
 
 func _on_credit_pressed():
 	get_tree().change_scene_to_file("res://MENU/Credit.tscn")
+
+
+func _on_story_pressed():
+	get_tree().change_scene_to_file("res://MENU/Story.tscn")
